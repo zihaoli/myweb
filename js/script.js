6275458abaeb61d0a533f5f2e629a62c0ae1b09a
zihaoli <vip100546@163.com>
2015-09-07 16:22:46 +0800

    d

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-var words = "hello word";
-console.log(words);
-//注释
+//var words = "hello word";
+//console.log(words);
+////注释
+var alertMessage = function (message) {
+    alert(message);
+}
+alertMessage('ninhao');
